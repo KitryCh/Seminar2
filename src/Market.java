2205306f51bd1fff80f4c1a84e848e3932391185
KitryCh <78431641+KitryCh@users.noreply.github.com>
2024-01-31 12:05:37 +0300

    Update Market.java

--- src/Market.java
+++ src/Market.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class Market {
+class Market implements MarketBehaviour {
+    private QueueBehaviour queue;
+    private String currentOrder;
+
+    public Market(QueueBehaviour queue) {
+        this.queue = queue;
+    }
+
+    @Override
+    public void acceptOrder(String order) {
+        System.out.println("Принят заказ: " + order);
+        queue.enqueue(order);
+    }
+
+    @Override
+    public String serveOrder() {
+        currentOrder = queue.dequeue();
+        System.out.println("Обслужен заказ: " + currentOrder);
+        return currentOrder;
+    }
+
+    // Метод для обновления состояния магазина
+    public void update() {
+        System.out.println("Текущий заказ: " + currentOrder);
+        System.out.println("Количество заказов в очереди: " + queue.size());
+    }
 }
