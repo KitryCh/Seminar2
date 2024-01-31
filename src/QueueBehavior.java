7120ec00dbeb3fd9fb70eb4b2a7cf75751e993c3
KitryCh <78431641+KitryCh@users.noreply.github.com>
2024-01-31 12:07:46 +0300

    Update QueueBehavior.java

--- src/QueueBehavior.java
+++ src/QueueBehavior.java
@@ -1,2 +1,5 @@
-package PACKAGE_NAME;public class QueueBehavior {
+interface QueueBehaviour {
+    void enqueue(String person);  // Добавить человека в очередь
+    String dequeue();  // Удалить и вернуть первого человека из очереди
+    int size();  // Получить размер очереди
 }
