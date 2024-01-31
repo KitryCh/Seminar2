8af984fe53da0a501e6321c8dccabcc661993c3f
KitryCh <78431641+KitryCh@users.noreply.github.com>
2024-01-31 12:07:01 +0300

    Update MarketBehavior.java

--- src/MarketBehavior.java
+++ src/MarketBehavior.java
@@ -1,36 +1,5 @@
-import java.util.LinkedList;
-
-class Queue implements QueueBehaviour {
-    private LinkedList<String> queue;
-
-    public Queue() {
-        queue = new LinkedList<>();
-    }
-
-    @Override
-    public void enqueue(String person) {
-        queue.add(person);
-        System.out.println("Добавлен в очередь: " + person);
-    }
-
-    @Override
-    public String dequeue() {
-        if (!isEmpty()) {
-            String person = queue.removeFirst();
-            System.out.println("Удален из очереди: " + person);
-            return person;
-        } else {
-            System.out.println("Очередь пуста");
-            return null;
-        }
-    }
-
-    @Override
-    public int size() {
-        return queue.size();
-    }
-
-    public boolean isEmpty() {
-        return queue.isEmpty();
-    }
+interface MarketBehaviour {
+    void acceptOrder(String order);  // Принять заказ
+    String serveOrder();  // Обслужить заказ и вернуть его
+    void update();
 }
